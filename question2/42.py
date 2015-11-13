275f5b715b1943f7db8401a86f6afeec2f5999d9
swapnil3667 <aswap1989@gmail.com>
2015-11-13 19:31:24 +0800

    Create 42.py

---
+++ question2/42.py
@@ -0,0 +1,15 @@
+def print_42(a):
+  d = {}
+  for i in range(len(string)):
+      if a[i] not in d:
+          d[a[i]] = 0
+      else
+          d[a[i]] = d[a[i]] + 1
+   cnt = 0
+   for k in d:
+       if d[k] != cnt:
+           return -1
+           break
+       cnt = cnt+1
+
+    return 42
